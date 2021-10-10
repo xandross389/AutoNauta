26a6b0efeb873e20ab9bd7807e5ab09e4dce800e
Alejandro Gómez Jiménez <xandross389@gmail.com>
2021-10-10 18:21:54 -0400

    - Moved utils methods to nautasdk.utils - Changed ping method used from utils to NautaProtocol class - Deleted utils.py file

--- nautasdk/utils.py
+++ nautasdk/utils.py
@@ -32,6 +32,7 @@ def val_or_error(callback):
     except Exception as ex:
         return ex.args[0]
 
+
 # define our clear function
 def clear():
     # for windows
@@ -42,30 +43,6 @@ def clear():
         _ = system('clear')
 
 
-# def str2time(strtime, format=None):
-#     # # Function to convert string to datetime
-#     # format = '%b %d %Y %I:%M%p'  # The format
-#     # datetime_str = datetime.datetime.strptime(date_time, format)
-#     #
-#     #     return datetime_str
-#     #
-#     # # Driver code
-#     # date_time = 'Dec 4 2018 10:07AM'
-#     # print(convert(date_time))
-#     import time
-#
-#     # Function to convert string to datetime
-#     datetime_str = time.mktime(strtime)
-#
-#     format = format or "%H:%M"  # The format
-#     dateTime = time.strftime(format, time.gmtime(datetime_str))
-#     return dateTime
-#
-#     # Driver code
-#     # date_time = (2018, 12, 4, 10, 7, 00, 1, 48, 0)
-#     # print(convert(date_time))
-
-
 def is_time_between(begin_time, end_time, check_time=None):
     # If check time is not given, default to current UTC time
     # check_time = check_time or datetime.utcnow().time()
@@ -73,6 +50,7 @@ def is_time_between(begin_time, end_time, check_time=None):
         check_time = check_time.strftime("%H:%M")
     else:
         check_time = datetime.now().time().strftime("%H:%M")
+    # check_time = check_time or datetime.now().time().strftime("%H:%M")
 
     if begin_time < end_time:
         return begin_time <= check_time <= end_time
